d8ed5a3ba5f6d140a974b56ed3d143b9ae405fbd
Toubassi <emil_toubassi@hotmail.com>
2026-09-10 13:18:34 +0300

    js/index.html

--- webfundemental/js/script.js
+++ webfundemental/js/script.js
@@ -1,19 +1,30 @@
 function pizzaOven(crust, sauce, cheese, toppings) {
-    var pizza = {
-        crustType: crust,
-        sauceType: sauce,
-        cheeseType: cheese,
-        toppings: toppings
-    };
-    return pizza;
+  var pizza = {};
+  pizza.crustType = crust;
+  pizza.sauceType = sauce;
+  pizza.cheeseType = cheese;
+  pizza.toppings = toppings;
+  return pizza;
 }
 
-var p1 = pizzaOven("deep dish", "traditional", "mozzarella", ["pepperoni", "sausage"]);
-var p2 = pizzaOven("hand tossed", "marinara", ["mozzarella", "feta"], ["mushrooms", "olives", "onions"]);
-var p3 = pizzaOven("thin crust", "pesto", "goat cheese", ["spinach", "sun-dried tomatoes"]);
+var p1 = pizzaOven("deep dish", "traditional", "mozzarella", ["pepperoni", "sausage"])
+
+
+var p2 = pizzaOven(
+  "hand tossed",
+  "marinara",
+  ["mozzarella", "feta"],
+  ["mushrooms", "olives", "onions"],
+);
+
+var p3 = pizzaOven("thin crust", "pesto", "goat cheese", [
+  "spinach",
+  "sun-dried tomatoes",
+]);
+
 var p4 = pizzaOven("stuffed crust", "alfredo", "cheddar", "pineapple");
 
-console.log(p1);
-console.log(p2);
-console.log(p3);
-console.log(p4);
+console.log("My First Pizza is: ", p1);
+console.log("My Second Pizza is: ", p2);
+console.log("My Third Pizza is: ", p3);
+console.log("My Fourth Pizza is: ", p4);
