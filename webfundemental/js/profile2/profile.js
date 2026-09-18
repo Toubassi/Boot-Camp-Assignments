9b80ab9b342fae680a4eb61d2fad35652a9357ed
Toubassi <emil_toubassi@hotmail.com>
2026-09-18 14:01:10 +0300

    profile2

--- webfundemental/js/profile2/profile.js
+++ webfundemental/js/profile2/profile.js
@@ -2,9 +2,7 @@ function removeRequest(element) {
   element.closest(".conPeople").remove();
 }
 
-// document.getElementById("userName")
-
-function editProfile(){
-var newName = prompt("Enter your new name:");
-document.getElementById("userName").textContent = newName;
+function editProfile() {
+  var newName = prompt("Enter your new name:");
+  document.getElementById("userName").textContent = newName;
 }
